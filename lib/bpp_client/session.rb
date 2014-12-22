1b079e9ff5fe20e88b004ea053abd50ac479397c
Andre Rodrigues <andre.rodrigues@tagview.com.br>
2014-12-22 17:33:32 -0200

    Implementando repository para session e testes

--- lib/bpp_client/session.rb
+++ lib/bpp_client/session.rb
@@ -1,14 +1,28 @@
-class BppClient::Session < ActiveResource::Base
+class BppClient::Session
   class InvalidCredentials < StandardError; end
+  class Repository < ActiveResource::Base
+    self.site = "http://localhost:3000/api/v2"
+    self.collection_name = "authentication"
 
-  self.site = "http://localhost:3000/api/v2"
-  self.collection_name = "authentication"
+    def save
+      create
+    end
+  end
 
   attr_reader :token
 
+  def initialize(email:, password:)
+    @email = email
+    @password = password
+  end
+
+  def repository
+    @repository ||= Repository.new(email: @email, password: @password)
+  end
+
   def save
-    response = create.body
-    token = JSON.parse(response)["token"]
+    response = repository.save
+    @token = JSON.parse(response.body)["token"]
   rescue ActiveResource::ResourceInvalid
     raise InvalidCredentials
   end
--- spec/bpp_client/session_spec.rb
+++ spec/bpp_client/session_spec.rb
@@ -1,9 +1,42 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
+require "spec_helper"
 
 describe BppClient::Session do
+  let(:email) { "teste@bpp.com.br" }
+  let(:password) { "password" }
+  let(:session_repository) { instance_spy BppClient::Session::Repository }
 
-  it "Ble" do
-    true
+  subject { BppClient::Session.new(email: email, password: password) }
+
+  before { allow(subject).to receive(:repository).and_return session_repository }
+
+  it "has a blank token" do
+    expect(subject).to respond_to(:token)
+    expect(subject.token).to be_nil
   end
-  
+
+  describe "#save" do
+    context "with valid credentials" do
+      let(:token) { "abc123" }
+      let(:valid_response) { double Net::HTTPResponse, body: { "token" => token }.to_json }
+
+      before do
+        allow(session_repository).to receive(:save).and_return valid_response
+        subject.save
+      end
+
+      it "saves the token" do
+        expect(subject.token).to eq token
+      end
+    end
+
+    context "with invalid credentials" do
+      before do
+        allow(session_repository).to receive(:save).and_raise ActiveResource::ResourceInvalid.new("Error")
+      end
+
+      it "raises InvalidCredentials error" do
+        expect { subject.save }.to raise_error(BppClient::Session::InvalidCredentials)
+      end
+    end
+  end
 end
